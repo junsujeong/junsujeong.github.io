77fcced17cea70721dcd7418e1dc3d3d8f8f10f9
junsu jeong <jeongj@uwplatt.edu>
2020-01-12 00:09:18 -0600

    Updates

---
+++ precache-manifest.011251817e9cd3433578067af11fc7de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2853bc08f6925579cb42afc4a8fcd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eacd820e344220f7e3b3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "13f352de9a14506517e4",
+    "url": "/static/js/2.c7e52266.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c7e52266.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eacd820e344220f7e3b3",
+    "url": "/static/js/main.793bd2ff.chunk.js"
+  },
+  {
+    "revision": "e45c4c5b295b79341b6c",
+    "url": "/static/js/runtime-main.f2ddc9b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
