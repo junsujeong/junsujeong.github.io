28fd37f124d80b874ca874575be6a7ef6787bb8f
junsu jeong <jeongj@uwplatt.edu>
2020-01-13 22:43:57 -0600

    More updates on about me

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,4 +114,66 @@
 
 			});
 
+    var $model = {
+        $skillCounter: 0,
+    }
+
+    var app = {
+
+        init: function(){
+            this.cacheDOM();
+            this.bindEvents();
+            this.toggleScroll();
+            this.showSkill(model.skillCounter);
+        },
+
+        cacheDOM: function(){
+            this.$skill      = document.getElementsByClassName('skill');
+            this.$skillArrow = $('.slider-arrow');
+            this.$navOverlay = $('.nav-overlay');
+            this.$toggleNav  = $('.toggle-nav');
+        },
+
+        bindEvents: function(){
+            this.$skillArrow.on('click', this.skillSlider.bind(this));
+            this.$toggleNav.on('click', this.toggleNav.bind(this));
+            $(window).scroll(this.toggleScroll);
+            $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(this.smoothScroll);
+        },
+
+        toggleScroll: function(){
+            if ($(document).scrollTop() > 0) {
+                $('nav').addClass('nav-scroll');
+            } else {
+                $('nav').removeClass('nav-scroll');
+            }
+
+            var scrollBarLocation = $(this).scrollTop();
+            $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').each(function(){
+                var sectionOffset = $(this.hash).offset().top;
+                if (Math.floor(sectionOffset) <= scrollBarLocation) {
+                    $(this).addClass('active');
+                    $(this).siblings().removeClass('active');
+                } else {
+                    $(this).removeClass('active');
+                }
+            });
+        },
+
+        showSkill: function(j){
+            var length = this.$skill.length;
+            for (var i = 0; i < length; i++) {
+                $(this.$skill[i]).hide();
+            }
+            $(this.$skill[j]).show();
+        },
+
+        skillSlider: function(e){
+            var i = $(e.target).attr('value');
+            model.skillCounter += Number(i);
+            if (model.skillCounter < 0) {
+                model.skillCounter = this.$skill.length - 1;
+            }
+            this.showSkill(model.skillCounter % this.$skill.length);
+        },
 })(jQuery);
